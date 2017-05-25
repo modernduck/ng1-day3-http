26cd6fc3ef026cf27b19eb9cfc70226ab39ca6b8
Sompop Kulapalanont <sompopkulapalanont@Sompops-MacBook-Pro.local>
2017-05-25 10:24:07 +0700

    add routing

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-  angular.module('myHttp', [])
-    .controller("MainController", function($scope){
+  angular.module('myHttp', ['ngRoute'])
+    .config(function($routeProvider){
+
+        $routeProvider.when("/", {
+            templateUrl:"pages/main.html",
+            controller:"MainController"
+        }).when("/phone", {
+            templateUrl:"pages/info.html"
+        })
+
+    })
+    .controller("MainController", function($scope, $http){
         $scope.message = "Hi"
+        $http({
+            method : "GET",
+            url : "phones/phones.json"
+        }).then(
+            successResponse => {
+                console.log(successResponse)
+                $scope.data = successResponse;
+            },
+            errorResponse => {}
+        )
     })
     
